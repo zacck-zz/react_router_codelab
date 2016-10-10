dd61827f197084abe87232b85095983a0b044043
Zacck <zacck@silke-lenovo.kd.local>
2016-10-10 10:33:27 +0200

    work on new components and visiting them from url

--- app/app.jsx
+++ app/app.jsx
@@ -1,24 +1,13 @@
-//import react
-var React = require('react');
-var ReactDOM = require('react-dom');
-//ES6 Destructuting syntax
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-//add components
-var App = require('App');
-import About from 'About';
-import Main from 'Main';
-
-
-//app css require
-require('style!css!sass!applicationStyles');
-
-//Create our Router
-ReactDOM.render(    //pass two args, JSX and the app element
+import React from 'react'
+import {render} from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import App from 'App'
+import About from 'About'
+import Main from 'Main'
+render((
   <Router history={hashHistory}>
-   {/*Main component will always be rendered*/}
-   <Route path="/" component={App}>
-     {/*Add Routes here */}
-   </Route>
- </Router>,
-  document.getElementById('app') //where to render
-);
+    <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+    <Route path="/main" component={Main}/>
+  </Router>
+), document.getElementById('app'))
