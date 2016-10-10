2229c54bbab1e8c5681cd43ace02b055b6877cac
Zacck <zacck@silke-lenovo.kd.local>
2016-10-10 10:38:25 +0200

    worked on using Link for navigation

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,18 @@
 var React = require('react');
+import {Link} from 'react-router';
 
 var App = React.createClass({
     render: function() {
        return(
                <div>
+                 <ul role="nav">
+                   <li>
+                     <Link to="/about">About</Link>
+                   </li>
+                   <li>
+                     <Link to="/main">Main</Link>
+                   </li>
+                 </ul>
                 <p>App  Component</p>
                </div>
              );
